6b3ed93bf920d24b135b1f293d2f9b0eeef1d260
youssefbazdeh <75418367+youssefbazdeh@users.noreply.github.com>
2023-11-10 14:39:53 +0100

    test class fix

--- src/test/java/tn/esprit/stationski/moniteur/service/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/stationski/moniteur/service/GestionStationSkiApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.stationski;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
